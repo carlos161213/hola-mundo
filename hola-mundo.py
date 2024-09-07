84587d273f99d435db519e2dcb1e98e4804a0a1c
Raimundo Alegría <raimundoangel.alegria@bbva.com>
2024-09-07 18:53:36 +0200

    First commit

---
+++ hola-mundo.py
@@ -0,0 +1,6 @@
+print("¡Hola mundo!") #Escribe cosas en la pantalla
+print("¿como te llamas?") # Aqui estoy pidiendo amablemente el nombre
+
+my_name = input() # Guardo el nombre que me ha dado
+
+print(f"encantado de conocerte {my_name}") # Imprimo el resultado
---
+++ sumar-dos-numeros.py
@@ -0,0 +1,11 @@
+print("hola")
+
+print("pon el primer numero")
+primer_numero = input()
+
+print("pon el segundo numero")
+segundo_numero = input()
+
+resultado = int(primer_numero) + int(segundo_numero)
+
+print(f"el resultado de la operacion es {resultado}")
